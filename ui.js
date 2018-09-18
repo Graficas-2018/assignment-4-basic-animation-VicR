18ef8171067b700ab481693895b97c6968f08db3
Victor Rendon <victor.rs@eucledian.mx>
2018-09-17 21:35:30 -0500

    monster animation

---
+++ ui.js
@@ -0,0 +1,8 @@
+/*
+  Three.js Basic Animation
+  Víctor Rendón Suárez | A01022462
+  17/09/2018
+*/
+function initControls() {
+  orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+}
